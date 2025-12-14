6d7b99d7cd0630bcac20cc3207e892229155337f
sr0116 <kysy0116@gmail.com>
2025-12-15 02:06:08 +0900

    공용 컴포넌트 제작중

--- src/app/components/ui/Input.tsx
+++ src/app/components/ui/Input.tsx
@@ -11,12 +11,61 @@ type InputProps = {
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function Input({
-  label,
-  error,
-  ...props}: InputProps) {
+                                label,
+                                error,
+                                ...props
+                              }: InputProps) {
   return (
     <div className="space-y-1">
-    {/*  label 영역*/}
+      {/*  label 영역*/}
+      {label && (
+        <label className="
+          text-sm
+          text-text-muted-light
+          dark:text-text-muted-dark
+          ">
+          {label}
+        </label>
+      )}
+      {/*  input 영역 */}
+      <input
+        {...props}
+        className={`
+        w-full h-11
+        rounded-md
+        // 배경색
+        bg-white
+        dark:bg-background-dark
+        // 테두리
+        border
+        border-border-light
+        dark:border-border-dark
+        // 내부 여백
+        px-3
+        text-sm
+        
+        transition-colors
+        duration-150
+        ease-in-out
+        
+        focus:outline-none
+        focus:ring-1
+        focus:ring-accent
+        
+        // 비활성화 상태
+        disabled:opacity-50
+        disabled:cursor-not-allowed
+        // 에러
+        ${error ? "border-red-500" : ""}
+        `}
+      />
+
+      {/*  error 메세지*/}
+      {error && (
+        <p className="text-xs text-red-500">
+          {error}
+        </p>
+      )}
     </div>
   )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,29 +5,52 @@
 import Button from "@/app/components/ui/Button";
 import Input from "@/app/components/ui/Input";
 import Card from "@/app/components/ui/Card";
+import {useState} from "react";
 
 export default function LoginPage() {
+
+  // ===== 입력값 상태 =====
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // ===== 에러 상태 =====
+  const [emailError, setEmailError] = useState<string | undefined>();
+  const [passwordError, setPasswordError] = useState<string | undefined>();
+
   return (
     <div
       className="
     min-h-[calc(100vh-64px)]
     bg-background-light dark:bg-background-dark
-    flex items-center justify-center
+    flex items-start justify-center
     px-4
+    pt-28
   "
     >
-
-    <div className="w-full max-w-[420px]">
+      <div className="w-full max-w-[420px]">
+        <h1 className="text-2xl font-semibold text-center mb-8">
+          로그인
+        </h1>
         <Card className="p-8 sm:p-10">
-          {/* 타이틀 */}
-          <h1 className="text-2xl font-semibold text-center mb-6">
-            로그인
-          </h1>
 
           {/* 폼 */}
           <div className="space-y-4">
-            <Input label="이메일" />
-            <Input label="비밀번호" type="password" />
+            <Input
+              label="이메일"
+              placeholder="example@flocut.io"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={emailError}
+            />
+
+            <Input
+              label="비밀번호"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              error={passwordError}
+            />
+
 
             <div className="text-right text-sm">
               <a
